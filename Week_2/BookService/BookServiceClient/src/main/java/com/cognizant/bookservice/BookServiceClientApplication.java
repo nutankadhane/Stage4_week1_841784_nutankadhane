c0ae5d83ecdf6d8b450ce3986486a6a6bd3ba995
Yashika Yadav <yashika.yadav@cognizant.com>
2020-06-23 00:31:43 +0530

    23jun

---
+++ Week_2/BookService/BookServiceClient/src/main/java/com/cognizant/bookservice/BookServiceClientApplication.java
@@ -0,0 +1,15 @@
+package com.cognizant.bookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class BookServiceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookServiceClientApplication.class, args);
+	}
+
+}
